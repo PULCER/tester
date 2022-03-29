b67d9f60e61a4fabf99f9df993ae06dacb094fa0
Anthony Howell <anthony@pulcer.net>
2022-03-28 18:48:21 -0700

    swaglet

--- src/tester.js
+++ src/tester.js
@@ -21,24 +21,39 @@ export function TesterMan() {
         console.log(eventOddsValue)
     }
 
+    const clear = () => {
+        setEventOddsValue([
+            { title: "Event 1 = ", id: 0, value: "" },
+            { title: "Event 2 = ", id: 1, value: "" },
+            { title: "Event 3 = ", id: 2, value: "" },
+            { title: "Event 4 = ", id: 3, value: "" },
+            { title: "Event 5 = ", id: 4, value: "" },
+            { title: "Event 6 = ", id: 5, value: "" },
+            { title: "Event 7 = ", id: 6, value: "" },
+            { title: "Event 8 = ", id: 7, value: "" },
+        ])
+    }
 
     return (
         <div>
-            {eventOddsValue.map((event) => (
-                <div key={event.id}>
-                    <label>
-                        {event.title}
-                        <input
-                            type="text"
-                            onChange={(e) => {
-                                handleEventChange(e, event.id)
-                            }}
-                        >
-                        </input>
-                    </label>
-                </div>
-            )
-            )}
+            <div>
+                {eventOddsValue.map((event) => (
+                    <div key={event.id}>
+                        <label>
+                            {event.title}
+                            <input
+                                type="text"
+                                onChange={(e) => {
+                                    handleEventChange(e, event.id)
+                                }}
+                            >
+                            </input>
+                        </label>
+                    </div>
+                )
+                )}
+            </div>
+            <button onClick={clear}>clear</button>
         </div>
     )
 }
